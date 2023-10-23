4db7215e9cd46bd7cdd15e17efe622e043b54bd1
Rahul Sharma <sharmarahul8848@gmail.com>
2023-10-23 10:45:40 +0530

    Final commit

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 8000;
-
-app.listen(port, function (err) {
-  if (err) console.log(`Error starting the error ${err}`);
-  console.log(`Server running on ${port}`);
-});
---
+++ models/employee.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+module.exports = Employee;
